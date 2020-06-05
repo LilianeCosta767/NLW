024ffc821cc96b34a5ac382f5e854e72c40a380c
LilianeCosta767 <lilianecosta767@hotmail.com>
2020-06-04 22:20:42 -0300

    metodos get e post simples no server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,26 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
 
+// lista todos os usuarios (por enquanto de forma estatica)
 app.get('/users', (request, response) => {
     console.log('listagem de usuarios');
 
-    response.send('Hello World');
+    response.json([
+        'Diego',
+        'Cleiton',
+        'Robson'
+    ]);
+});
+
+// cria usuarios
+app.post('/users', (request, response) => {
+    const user = {
+        name: 'Diego',
+        email: 'diego@rocketseat.com.br'
+    }
+
+    return response.json(user);
 });
 
 app.listen(3333);
