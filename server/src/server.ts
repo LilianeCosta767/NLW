3f9020a2cb70157a665461186de271656993b6e9
LilianeCosta767 <lilianecosta767@hotmail.com>
2020-06-10 18:07:44 -0300

    insercao de imagens

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json()); // use serve pra colocar um plugin no express
-
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
 
@@ -13,3 +15,4 @@ app.listen(3333);
 // request param: parametros que vem na propria rota que identificam um recurso, muito utilizado quando queremos buscar/atualizar um UNICO usuario, etc
 // query param: parametros na rota que serve para filtrar, fazer paginação. não é um parametro obrigatorio.
 // request body: parametros para criação/atualização de informações
+// o express.static() é para servir materiais estaticos de uma pasta especifica
